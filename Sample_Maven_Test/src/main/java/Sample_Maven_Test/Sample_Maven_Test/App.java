9412dcccfd9b7961d87bc1a4d7500b9b3cc7c705
Mallarapukoushik <mallarapu.koushik@wipro.com>
2026-06-23 10:56:54 +0530

    Maven commit

---
+++ Sample_Maven_Test/src/main/java/Sample_Maven_Test/Sample_Maven_Test/App.java
@@ -0,0 +1,10 @@
+package Sample_Maven_Test.Sample_Maven_Test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
